2ce867110a0f7f5f5eb47a50a94f1eeb89c67fc5
Ali Salehi <alisalehi13800930@gmail.com>
2024-02-11 23:14:54 +0330

    Add(Route): login & logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\Web\Auth\LoginController;
+use App\Http\Controllers\Web\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('auth/login', LoginController::class);
+Route::post('auth/logout', LogoutController::class);
